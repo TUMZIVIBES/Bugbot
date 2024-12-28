65a21f1cc72acf119901def8e0058a40504caceb
TUMZIVIBES <adeyemotumininu437@gmail.com>
2024-12-28 15:12:16 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  BOT_TOKEN: "7229944975:AAGetyMH46tm4QUnmZ-3oIUjlJudntUd7Ok", // Token bot Telegram
+  OWNER_ID: "6857250857", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
+
+
